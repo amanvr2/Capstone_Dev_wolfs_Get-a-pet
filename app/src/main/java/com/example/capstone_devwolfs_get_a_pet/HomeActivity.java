b7d6904407b3a37dfa1d8f2c92f695610f5f6f1a
amanvr2 <amanvr2@gmail.com>
2021-06-30 17:22:13 -0400

    home activity UI added

---
+++ app/src/main/java/com/example/capstone_devwolfs_get_a_pet/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.example.capstone_devwolfs_get_a_pet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
