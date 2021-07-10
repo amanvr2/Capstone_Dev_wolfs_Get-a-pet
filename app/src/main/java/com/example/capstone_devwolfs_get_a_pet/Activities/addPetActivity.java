2fe084458a52cfc6f732402cdf71b0043d91cc83
simranSodhi <smrnsodhi@gmail.com>
2021-07-10 11:50:30 -0400

    add pet activity added

---
+++ app/src/main/java/com/example/capstone_devwolfs_get_a_pet/Activities/addPetActivity.java
@@ -0,0 +1,16 @@
+package com.example.capstone_devwolfs_get_a_pet.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.capstone_devwolfs_get_a_pet.R;
+
+public class addPetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_pet);
+    }
+}
