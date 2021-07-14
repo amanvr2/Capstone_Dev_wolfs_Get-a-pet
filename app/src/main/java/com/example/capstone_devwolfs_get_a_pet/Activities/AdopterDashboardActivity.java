142457a037a0ff3f0791501574323724e99d7351
Saksham Arora <sakshamarora0012@gmail.com>
2021-07-14 17:10:01 +0530

    Adopter Activity

--- app/src/main/java/com/example/capstone_devwolfs_get_a_pet/Activities/AdopterDashboardActivity.java
+++ app/src/main/java/com/example/capstone_devwolfs_get_a_pet/Activities/AdopterDashboardActivity.java
@@ -5,30 +5,33 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.capstone_devwolfs_get_a_pet.Activities.Adopter.FindPetActivity;
+import com.example.capstone_devwolfs_get_a_pet.Activities.Adopter.FindShelterActivity;
 import com.example.capstone_devwolfs_get_a_pet.R;
 
 public class AdopterDashboardActivity extends AppCompatActivity {
 
 
-    Button wishlistBtn,profileBnt,sheltersBtn,donationBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adopter_dashboard);
 
-        wishlistBtn = findViewById(R.id.wishlistBtn);
-        profileBnt = findViewById(R.id.profileBtn);
-        sheltersBtn = findViewById(R.id.shelterBtn);
-        donationBtn = findViewById(R.id.donationBtn);
+
 
     }
 
-    public void profileBtnClick(View view) {
-        Intent intent = new Intent(view.getContext(), AdopterProfileActivity.class);
+    public void findShelter(View view) {
+        Intent intent = new Intent(AdopterDashboardActivity.this, FindShelterActivity.class);
+        startActivity(intent);
+
+    }
+
+    public void findPet(View view) {
+        Intent intent = new Intent(AdopterDashboardActivity.this, FindPetActivity.class);
         startActivity(intent);
     }
 }
