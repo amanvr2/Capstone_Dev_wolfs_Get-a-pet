7d7b01cbae96623070f89a78d230add1b8c4a4bf
Saksham Arora <sakshamarora0012@gmail.com>
2021-07-14 15:56:07 +0530

    Find Shelter UI

---
+++ app/src/main/java/com/example/capstone_devwolfs_get_a_pet/Activities/Adopters/FindShelterActivity.java
@@ -0,0 +1,16 @@
+package com.example.capstone_devwolfs_get_a_pet.Activities.Adopter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.capstone_devwolfs_get_a_pet.R;
+
+public class FindShelterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_shelter);
+    }
+}
