54c19f6ff1e5679dac5d0d5d8b2e07010d344fd3
Pedrofmattosb <pedrofmattosb@gmail.com>
2021-07-14 05:13:03 -0300

    Adopter Profile Almost Ready

--- app/src/main/java/com/example/capstone_devwolfs_get_a_pet/Activities/AdopterDashboardActivity.java
+++ app/src/main/java/com/example/capstone_devwolfs_get_a_pet/Activities/AdopterDashboardActivity.java
@@ -2,7 +2,10 @@ package com.example.capstone_devwolfs_get_a_pet.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.capstone_devwolfs_get_a_pet.R;
@@ -10,11 +13,22 @@ import com.example.capstone_devwolfs_get_a_pet.R;
 public class AdopterDashboardActivity extends AppCompatActivity {
 
 
+    Button wishlistBtn,profileBnt,sheltersBtn,donationBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adopter_dashboard);
 
+        wishlistBtn = findViewById(R.id.wishlistBtn);
+        profileBnt = findViewById(R.id.profileBtn);
+        sheltersBtn = findViewById(R.id.shelterBtn);
+        donationBtn = findViewById(R.id.donationBtn);
 
+    }
+
+    public void profileBtnClick(View view) {
+        Intent intent = new Intent(view.getContext(), AdopterProfileActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/capstone_devwolfs_get_a_pet/Activities/AdopterProfileActivity.java
@@ -0,0 +1,41 @@
+package com.example.capstone_devwolfs_get_a_pet.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.EditText;
+
+import com.example.capstone_devwolfs_get_a_pet.R;
+
+public class AdopterProfileActivity extends AppCompatActivity {
+
+    SharedPreferences sharedPreferences;
+    EditText adopterName,adopterEmail,adopterPhone,adopterAddress,adopterDescription;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adopter_profile);
+
+        sharedPreferences = getSharedPreferences("Adopter",  Context.MODE_PRIVATE);
+
+        adopterName = findViewById(R.id.adopterNameTv);
+        adopterEmail = findViewById(R.id.adopterEmailTv);
+        adopterPhone = findViewById(R.id.adopterPhoneTv);
+        adopterAddress = findViewById(R.id.adopterAddressTv);
+        adopterDescription = findViewById(R.id.adopterDescriptionTv);
+
+
+
+        adopterName.setText(sharedPreferences.getString("Name","Adopter Name"));
+        adopterEmail.setText(sharedPreferences.getString("Email","Adopter Email"));
+        adopterPhone.setText(sharedPreferences.getString("Phone","Adopter Phone"));
+        adopterAddress.setText(sharedPreferences.getString("Address","Adopter Address"));
+        adopterDescription.setText(sharedPreferences.getString("Description","Adopter Description"));
+
+
+
+    }
+}
